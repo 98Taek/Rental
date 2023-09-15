cbbf4272e3491180b5887e979a934268a9d57d86
Jeong Taek <imjt9345@naver.com>
2023-09-15 23:39:58 +0900

    psycopg2-binary 설치, postgresql 연결

--- rental/settings.py
+++ rental/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'rental.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rentaldb',
+        'USER': 'rental',
+        'PASSWORD': '1234',
     }
 }
 
