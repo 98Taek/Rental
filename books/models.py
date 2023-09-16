802109b6f780cac71e4c799d847886b6e2bc2b96
Jeong Taek <imjt9345@naver.com>
2023-09-17 00:15:31 +0900

    Review 모델 추가, 마이그레이션

--- books/models.py
+++ books/models.py
@@ -32,3 +32,14 @@ class Rating(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [models.Index(fields=['created'])]
