97327274ba98b0b231394c7138338bf306b0f5d5
Jeong Taek <imjt9345@naver.com>
2023-09-16 17:02:19 +0900

    RatingForm 추가

--- books/forms.py
+++ books/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100, label='Search Books')
+
+
+class RatingForm(forms.Form):
+    rating = forms.DecimalField(max_digits=2, decimal_places=1, min_value=1.0, max_value=5.0)
